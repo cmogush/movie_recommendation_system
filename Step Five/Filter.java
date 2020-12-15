8c09643b7322b4eb5f1c76f60caf90f8421623ea
cmogush <cmogush@gmail.com>
2020-12-15 12:16:12 -0500

    Removed old files

--- Step Five/Filter.java
+++
@@ -1,4 +0,0 @@
-//returns true if the movie satisfies the criteria in the method and returns false otherwise
-public interface Filter {
-	public boolean satisfies(String id); //All filters that implement this interface must also have this method
-}
